42ed0412342df21ba2e1fc12144121d967db5a1e
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2022-04-10 09:30:35 +0200

    feat/access DB

--- API/index.php
+++ API/index.php
@@ -1,9 +1,29 @@
 <?php
 header('Content-Type: application/json');
 
-$flys = [];
+$host   = 'localhost';
+$db     = 'API';
+$admin  = 'root';
+$pw     = '';
+
+try
+{
+    $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8', $admin, $pw);
+    $flys["success"] = true;
+    $flys["message"] = "connection done";
+}
+catch(Exception $e)
+{
+    $flys["success"] = false;
+    $flys["message"] = "connection fail";
+}
+
+$request = $pdo->prepare("SELECT * FROM `flys`");
+$request->execute();
+
 $flys["success"] = true;
-$flys["message"] = "connected";
-$flys["results"]['fly'] = ["Paris-Toulouse", "Lyon-Paris"];
+$flys["message"] = "Liste des vols";
+$flys["results"]["flys"] = $request->fetchAll();
+// var_dump($result);
 
 echo json_encode($flys);
