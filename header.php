f88e1f814d212ac5ecac6d562df1b969238f0dc7
François CB <73296404+fransoa2103@users.noreply.github.com>
2022-04-11 08:20:04 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,15 @@
 <?php
 header('Content-Type: application/json');
 
-$host   = 'localhost';
-$db     = 'API';
-$admin  = 'root';
-$pw     = '';
+$host   = 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com';
+$db     = 'dc36rqs7b26ct4';
+$admin  = 'hssoqdizhvmubt';
+$pw     = 'c0e3dd3948831fa75ea66d82e2837aadc97730ffbe0226c1d8d9c68010fa62bb';
+$port   = '5432';
 
 try
 {
-    $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8', $admin, $pw);
+    $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';port='.$port.';charset=utf8', $admin, $pw);
     $flights["success"] = true;
     $flights["message"] = "connection done";
 }
