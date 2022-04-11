cf2fb740f82ea13c1e6fc7130b6e8d016667f654
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2022-04-11 12:12:13 +0200

    refresh main folder API and delete old folder

--- exo_api_PHP/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    header('Access-Control-Allow-Origin: *');
-    
-    $first_name     = (isset($_GET['first_name']) && $_GET['first_name'] != '') ? 'Hi dear, '.$_GET['first_name'] : 'data is missing';
-    $result         = $first_name;
-    $json_result    = json_encode(['first_name' => $first_name, 'result' => $result]);
-    
-    echo $result;
--- header.php
+++ header.php
@@ -1,23 +1,13 @@
 <?php
 header('Content-Type: application/json');
 
-//Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// // Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-
+// Get Heroku ClearDB connection information
 $host   = 'eu-cdbr-west-02.cleardb.net';
 $db     = 'heroku_f91335195b01cd2';
 $admin  = 'b85f1b8de2ecef';
 $pw     = 'b692b14a';
 
+// Connect to DB
 try {
     $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8', $admin, $pw);
     $flights["success"] = true;
