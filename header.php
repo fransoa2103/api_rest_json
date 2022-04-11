7066728040f4ca68daac9a22a0a73e4b854b630f
François CB <73296404+fransoa2103@users.noreply.github.com>
2022-04-11 08:38:09 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 header('Content-Type: application/json');
 
 $host   = 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com';
-$db     = 'dc36rqs7b26ct4';
+// $db     = 'dc36rqs7b26ct4';
+$db     = 'postgres://hssoqdizhvmubt:c0e3dd3948831fa75ea66d82e2837aadc97730ffbe0226c1d8d9c68010fa62bb@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dc36rqs7b26ct4';
 $admin  = 'hssoqdizhvmubt';
 $pw     = 'c0e3dd3948831fa75ea66d82e2837aadc97730ffbe0226c1d8d9c68010fa62bb';
 $port   = '5432';
