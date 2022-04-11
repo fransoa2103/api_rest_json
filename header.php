f41c3ea56d35bb55c59f958d0aed32d375650bb1
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2022-04-11 11:46:40 +0200

    refactor/header

--- header.php
+++ header.php
@@ -2,31 +2,27 @@
 header('Content-Type: application/json');
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
+// // Connect to DB
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
-// $host   = 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com';
-// $db     = 'dc36rqs7b26ct4';
-// $admin  = 'hssoqdizhvmubt';
-// $pw     = 'c0e3dd3948831fa75ea66d82e2837aadc97730ffbe0226c1d8d9c68010fa62bb';
-// $port   = '5432';
+$host   = 'eu-cdbr-west-02.cleardb.net';
+$db     = 'heroku_f91335195b01cd2';
+$admin  = 'b85f1b8de2ecef';
+$pw     = 'b692b14a';
 
-// try
-// {
-//     $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';port='.$port.';charset=utf8', $admin, $pw);
-//     $flights["success"] = true;
-//     $flights["message"] = "connection done";
-// }
-// catch(Exception $e)
-// {
-//     $flights["success"] = false;
-//     $flights["message"] = "connection fail";
-// }
+try {
+    $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8', $admin, $pw);
+    $flights["success"] = true;
+    $flights["message"] = "connection done";
+} catch (Exception $e) {
+    $flights["success"] = false;
+    $flights["message"] = "connection fail";
+}
