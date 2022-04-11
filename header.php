5726cdb24b9dc58532ff7ba721d243844dce4652
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2022-04-11 11:33:48 +0200

    feat/add transfer and connect db to heroku

--- header.php
+++ header.php
@@ -1,20 +1,32 @@
 <?php
 header('Content-Type: application/json');
 
-$host   = 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com';
-$db     = 'dc36rqs7b26ct4';
-$admin  = 'hssoqdizhvmubt';
-$pw     = 'c0e3dd3948831fa75ea66d82e2837aadc97730ffbe0226c1d8d9c68010fa62bb';
-$port   = '5432';
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-try
-{
-    $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';port='.$port.';charset=utf8', $admin, $pw);
-    $flights["success"] = true;
-    $flights["message"] = "connection done";
-}
-catch(Exception $e)
-{
-    $flights["success"] = false;
-    $flights["message"] = "connection fail";
-}
+
+// $host   = 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com';
+// $db     = 'dc36rqs7b26ct4';
+// $admin  = 'hssoqdizhvmubt';
+// $pw     = 'c0e3dd3948831fa75ea66d82e2837aadc97730ffbe0226c1d8d9c68010fa62bb';
+// $port   = '5432';
+
+// try
+// {
+//     $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';port='.$port.';charset=utf8', $admin, $pw);
+//     $flights["success"] = true;
+//     $flights["message"] = "connection done";
+// }
+// catch(Exception $e)
+// {
+//     $flights["success"] = false;
+//     $flights["message"] = "connection fail";
+// }
