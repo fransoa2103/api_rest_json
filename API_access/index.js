eefbbaab8d06668f117da3799bc68ecf7f818ce8
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2022-04-13 09:22:12 +0200

    feat/fold access-api/connexion text with JS Fetch mode/

--- API_access/index.js
+++ API_access/index.js
@@ -3,21 +3,22 @@ Test method FETCH from JavaScript
 ************************************/ 
 
 "use strict";
-const url = "https://fcb-test-api.herokuapp.com/?arrival=paris";
-// const url = "https://fcb-test-api.herokuapp.com";
 
-async function recupererPrix() {
-  const requete = await fetch(url, {
-    method: 'GET'
-  });
-  if(!requete.ok) {
-    alert('Un problème est survenu.');
-  } else {
-    let donnees = await requete.json();
-    console.log(donnees);
-    document.querySelector('span').textContent = donnees.results.flights[0]['arrival'];
-  }
+const url = "https://fcb-test-api.herokuapp.com/?arrival=paris";
+
+async function findFlightByArrival()
+{
+    const req = await fetch(url, {
+        method: 'GET'
+    });
+
+    if(!req.ok) {
+        alert('connexion failure!');
+    }
+    else {
+        let data = await req.json();
+        console.log(data);
+    }
 }
 
-// setInterval(recupererPrix, 1000);
-recupererPrix();
+findFlightByArrival();
