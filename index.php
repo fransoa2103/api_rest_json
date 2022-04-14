6b975c9197db339aa7b83442729189c2c11a4342
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2022-04-14 07:02:15 +0200

    fix/index.php/isset() like boolean response

--- index.php
+++ index.php
@@ -6,26 +6,30 @@ header('Content-Type: application/json');
 require_once 'Flight.php';
 
 // var_dump($_GET);
+// var_dump(isset($_GET['arrival']),$_GET['depart']);die;
 
-if (isset($_GET) && count($_GET) > 0){
-    if (isset($_GET['arrival']) !== null && isset($_GET['depart']) !== null)
+if (isset($_GET) && count($_GET) > 0)
+{
+    if (count($_GET)>1)
     {
-        flightListByDepartArrival($_GET['depart'], $_GET['arrival']);
-    }
-
-   else if (isset($_GET['arrival']) !== null)
+        if ((isset($_GET['arrival']) === TRUE) && (isset($_GET['depart']) === TRUE))
+        {
+            flightListByDepartArrival($_GET['depart'], $_GET['arrival']);
+        }
+    }   
+    else
     {
-        flightListByArrival($_GET['arrival']);
-    }
-
-   else if (isset($_GET['depart']) !== null)
-    {
-        flightListByDepart($_GET['depart']);
+        if (isset($_GET['arrival']) === TRUE)
+        {
+            flightListByArrival($_GET['arrival']);
+        }
+        else if (isset($_GET['depart']) === TRUE)
+        {
+            flightListByDepart($_GET['depart']);
+        }
     }
 }
 else
 {
     flightsAll();
 }
-
-    
